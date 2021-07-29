0ba99f4aba9515bb1fdeced216a2ea89d3ea4dd0
tokuyama shouta <toku1031ts@gmail.com>
2021-07-29 20:57:37 +0900

    forEach

---
+++ index.js
@@ -0,0 +1,30 @@
+//ES5 for
+
+let colors = ['red', 'blue', 'green'];
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+//forEach
+colors.forEach(function(color) {
+  console.log(color);
+})
+
+//数字の配列を用意
+let numbers = [1,2,3,4,5];
+//合計を保持する変数を用意する
+let sum = 0;
+//配列の1つ1つの合計をたす。
+numbers.forEach(function(number) {
+  sum += number;
+});
+//合計を表示する
+sum;
+
+posts.forEach(function(post){
+  savePost(post)
+})
+images.forEach(function(image){
+  areas.push(image.height*image.width);
+})
