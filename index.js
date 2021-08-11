cf96dd181c023d6406dff6cf33d4d4f77843e0eb
tokuyama shouta <toku1031ts@gmail.com>
2021-08-12 00:01:21 +0900

    アロー関数の使い所2

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ const team = {
   members: ['太郎','花子'],
   teamName: 'スーパーチーム',
   teamSummary: function() {
-    return this.members.map(function(member){
+    return this.members.map((member) => {
       return `${member}は${this.teamName}の所属です`;
     });
   }
