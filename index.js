d71cdbd547766458e0f8c8cab55071766728b088
tokuyama shouta <toku1031ts@gmail.com>
2021-08-13 21:07:51 +0900

    オブジェクトリテラル

--- index.js
+++ index.js
@@ -251,5 +251,25 @@ const team = {
   }
 }
 
+function createBookShop(inventory) {
+  return {
+    inventory,
+    inventoryValue() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceFprTitle(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  };
+}
+
+const inventory = [
+  {title: 'ハリーポッター',price: 1000},
+  {title: 'JavaScript入門',price: 1500},
+]
+
+const bookShop = createBookShop(inventory);
+bookShop.inventoryValue();
+bookShop.priceFprTitle('ハリーポッター');
 
 
